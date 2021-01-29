d12c72ddb37a8bf7ee9c38c0ff4342f6f4ac95e2
Pekka Korpinen <pekka.korpinen@iki.fi>
2021-01-29 20:04:57 +0200

    Refactor, implement retry

--- nr7101/nr7101.py
+++ nr7101/nr7101.py
@@ -1,64 +1,62 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import logging
-import base64
 import json
+import base64
 import requests
 import urllib3
 
 logger = logging.getLogger(__name__)
 
-def get_status(url, username, password, cookiefile=None):
+
+def get_status(url, username, password, params={}):
     password_b64 = base64.b64encode(password.encode('utf-8')).decode('utf-8')
-    login = '{"Input_Account":"%s","Input_Passwd":"%s","currLang":"en","RememberPassword":0,"SHA512_password":false}' % (username, password_b64)
+    login = {
+        'Input_Account': username,
+        'Input_Passwd': password_b64,
+        'currLang': 'en',
+        'RememberPassword': 0,
+        'SHA512_password': False,
+    }
+    login_json = json.dumps(login)
 
-    with requests.Session() as s:
-        # NR7101 is using by default self-signed certificates
-        s.verify = False
-        urllib3.disable_warnings()
+    # NR7101 is using by default self-signed certificates, so ignore the warnings
+    params['verify'] = False
+    urllib3.disable_warnings()
 
-        # Read cookiefile
-        if cookiefile:
-            try:
-                with open(cookiefile, 'rt') as f:
-                    cookies = json.load(f)
-                requests.utils.add_dict_to_cookiejar(s.cookies, cookies)
-                logger.debug("Loaded cookies")
-            except FileNotFoundError:
-                logger.debug("Cookie file does not exist, ignoring.")
-
-        # Check connection
-        r = s.get(url + '/getBasicInformation')
+    # Check connection
+    with requests.get(url + '/getBasicInformation', **params) as r:
         assert r.status_code == 200
         assert r.json()['result'] == 'ZCFG_SUCCESS', 'Connection failure'
 
-        # Login
-        r = s.post(url + '/UserLogin', data=login)
+    # Login
+    with requests.post(url + '/UserLogin', data=login_json, **params) as r:
         if r.status_code != 200:
             logger.error('Unauthorized')
             return
         sessionkey = r.json()['sessionkey']
 
-        # (optional)
-        r = s.get(url + '/UserLoginCheck')
+        # Update cookies
+        params['cookies'] = requests.utils.dict_from_cookiejar(r.cookies)
+
+    # Check login
+    with requests.get(url + '/UserLoginCheck', **params) as r:
         assert r.status_code == 200
 
-        # Get data
-        r = s.get(url + '/cgi-bin/DAL?oid=cellwan_status')
-        assert r.status_code == 200
+    # Get cellular status
+    with requests.get(url + '/cgi-bin/DAL?oid=cellwan_status', **params) as r:
+        if r.status_code == 504:
+            # NR7101 sometimes respons "Timeout"
+            raise TimeoutError
+        if r.status_code != 200:
+            logger.error('Unable to fetch cellwan_status. Status=%d %s', r.status_code, r.text)
+            raise ConnectionError
         j = r.json()
         assert j['result'] == 'ZCFG_SUCCESS'
         status = j['Object'][0]
 
-        # Logout
-        r = s.get(f'{url}/cgi-bin/UserLogout?sessionkey={sessionkey}')
+    # Logout
+    with requests.get(f'{url}/cgi-bin/UserLogout?sessionkey={sessionkey}', **params) as r:
         assert r.status_code == 200
-
-        # Write cookiefile
-        if cookiefile:
-            cookies = requests.utils.dict_from_cookiejar(s.cookies)
-            with open(cookiefile, 'wt') as f:
-                json.dump(cookies, f)
-            logger.debug("Cookies saved")
 
     return status
