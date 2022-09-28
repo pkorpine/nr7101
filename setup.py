653b706b5551ba9f6f06fa6d77d73855df8b7951
Pekka Korpinen <pekka.korpinen@iki.fi>
2022-09-28 21:21:12 +0300

    Fix flake8 warnings

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from nr7101.version import __version__
 
 setup(
-    name = "nr7101",
-    version = __version__,
-    description = "Zyxel NR7101 tool",
-    author = "Pekka Korpinen",
-    author_email = "pekka.korpinen@iki.fi",
-    license = "MIT",
-    url = "https://github.com/pkorpine/nr7101",
-    packages = ["nr7101"],
-    install_requires = [
+    name="nr7101",
+    version=__version__,
+    description="Zyxel NR7101 tool",
+    author="Pekka Korpinen",
+    author_email="pekka.korpinen@iki.fi",
+    license="MIT",
+    url="https://github.com/pkorpine/nr7101",
+    packages=["nr7101"],
+    install_requires=[
         "requests",
     ],
-    entry_points = {
+    entry_points={
         "console_scripts": ['nr7101-tool=nr7101.cli:cli'],
     },
 )
